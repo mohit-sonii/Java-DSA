c70c9bfa006c3b403893ee83a8770d218ff63895
Mohit Soni <mohitsoni9731@gmail.com>
2023-10-23 15:21:56 +0530

    Delete value at index

---
+++ Arrays/1D array/deletion.java
@@ -0,0 +1,39 @@
+
+
+//delete an element form 1D array
+
+import java.util.*;
+public class deletion
+{
+    public static void main(String ags[])
+    {
+        Scanner obj=new Scanner(System.in);
+        int num=obj.nextInt();
+        int a[]=new int[num];
+        int b[]=new int[num-1];
+        for(int i=0;i<num;i++)
+        {
+            a[i]=obj.nextInt();
+        }
+        System.out.println("Enter the index you want ot delete");
+        int value=obj.nextInt();
+        for(int i=0;i<a.length;i++)
+        {
+            if(i<value)
+            {
+                b[i]=a[i];
+            }
+            else if(i==value)
+            continue;
+            else 
+            {
+                b[i-1]=a[i];
+            }
+        }
+        System.out.println("REsult:");
+        for(int i=0;i<num-1;i++)
+        {
+            System.out.println(b[i]);
+        }
+    }
+}
