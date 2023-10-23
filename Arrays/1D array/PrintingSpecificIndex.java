f0b6856370b84f077586f6f778bd728eb80a659c
Mohit Soni <mohitsoni9731@gmail.com>
2023-10-23 15:41:53 +0530

    Finding the index of a parti. element

---
+++ Arrays/1D array/PrintingSpecificIndex.java
@@ -0,0 +1,28 @@
+
+
+// take an array from the user. search for a given number X and print the index at which it occurs.
+
+import java.util.*;
+public class PrintingSpecificIndex
+{
+    public static void main(String ags[])
+    {
+        Scanner obj=new Scanner(System.in);
+        int num=obj.nextInt();
+        int a[]=new int[num];
+        for(int i=0;i<num;i++)
+        {
+            a[i]=obj.nextInt();
+        }
+        System.out.println("Enter the number you want to find index of: ");
+        int value=obj.nextInt();
+        for(int i=0;i<num;i++)
+        {
+            if(a[i]==value)
+            {
+                System.out.println("Index of this value is: "+i);
+            }
+            
+        }
+    }
+}
