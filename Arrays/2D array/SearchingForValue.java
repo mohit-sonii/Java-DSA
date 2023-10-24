61148be4b5454fd5994bce704998a0fa8002c020
Mohit Soni <mohitsoni9731@gmail.com>
2023-10-24 11:21:39 +0530

    searching for a specfic value

---
+++ Arrays/2D array/SearchingForValue.java
@@ -0,0 +1,41 @@
+
+// take a matrix as input from user. search for a given number x and print the indices at twhich it 
+// occur
+
+import java.util.*;
+public class SearchingForValue
+{
+     public static void main(String ags[])
+     {
+          Scanner obj=new Scanner(System.in);
+          System.out.print("Enter the number of rows: ");
+          int rows=obj.nextInt();
+          System.out.print("Enter the number of cols: ");
+          int cols=obj.nextInt();
+          int a[][]=new int[rows][cols];
+          //inputting of array
+          for(int i=0;i<rows;i++)
+          {
+               for(int j=0;j<cols;j++)
+               {
+                    a[i][j]=obj.nextInt();
+               }
+          }
+          System.out.print("Enter the number you want ot find: ");
+          int num=obj.nextInt();
+          System.out.println("Index are: ");
+          //searching
+          for(int i=0;i<rows;i++)
+          {
+               for(int j=0;j<cols;j++)
+               {
+                    if(a[i][j]==num)
+                    {
+                         System.out.print(i+"  "+j);
+                    }
+               }
+               System.out.println();
+          }
+
+     }
+}
