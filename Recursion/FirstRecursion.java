cfc50e08879e191e2661b27f9587a323ba7170a8
Mohit Soni <mohitsoni9731@gmail.com>
2023-10-25 17:12:44 +0530

    First Recursion program

---
+++ Recursion/FirstRecursion.java
@@ -0,0 +1,18 @@
+
+//print number s from 5 to 1
+
+public class FirstRecursion
+{
+     public static void PrintNum(int n)
+     {
+          if(n==0)
+               return;
+          System.out.println(n);
+          PrintNum(n-1);
+     }
+     public static void main(String ags[])
+     {
+          int n=5;
+          PrintNum(n);
+     }
+}
