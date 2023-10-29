4f63d1d33ef174ece827b48d846c1cf0c48b713e
Mohit Soni <mohitsoni9731@gmail.com>
2023-10-29 11:54:48 +0530

    Finding n power of x

---
+++ Recursion/power.java
@@ -0,0 +1,24 @@
+
+
+public class power 
+{
+     public static int Calpow(int x,int n)
+     {
+          if(n==0)
+          {
+               return 1;
+          }
+          if(x==0)
+          {
+               return 0;
+          }
+          int xPower1=Calpow(x,n-1);
+          int xPowern=x*xPower1;
+          return xPowern;
+     }
+     public static void main(String agrs[])
+     {
+          int x=2,n=5;
+          System.out.println(Calpow(x,n));
+     }
+}
