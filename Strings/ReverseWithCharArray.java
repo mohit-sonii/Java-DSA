1f550802a9dbc2d595f07230719c86174b0701b6
Mohit Soni <mohitsoni9731@gmail.com>
2023-11-05 15:54:12 +0530

    Reverse a  string using Char Array-simplest method

---
+++ Strings/ReverseWithCharArray.java
@@ -0,0 +1,18 @@
+public class ReverseWithCharArray 
+{
+     public static String ReverseString(String str)
+     {
+          char a[]=str.toCharArray();
+          String reverse="";
+          for(int i=a.length-1;i>=0;i--)
+          {
+               reverse+=a[i];
+          }
+          return reverse;
+     }
+     public static void main(String ags[])
+     {
+          String str="java";
+          System.out.println(ReverseString(str));
+     }
+}
