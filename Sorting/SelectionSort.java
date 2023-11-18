484d799f832a1327912c89d97958c84574467fd1
Mohit Soni <mohitsoni9731@gmail.com>
2023-11-18 21:46:57 +0530

    Selection Sorting in java

--- Sorting/SelectionSort.java
+++ Sorting/SelectionSort.java
@@ -1,6 +1,7 @@
 package Sorting;
-//in this we have to find the smallest number from the whole list and put it in the first place
+//Selection sorting in java
 
+//in this we have to find the smallest number from the whole list and put it in the first place
 //then we find the second smallest number and put it in the 2nd place.
 //first we  will consider that the first element is the smallest one then we store it in the variable
 //and we will adjacently check whether smaller than preious number is available or not. If yes,
@@ -11,7 +12,7 @@ public class SelectionSort {
 
      public static void main(String sag[]) {
           int array[] = { 7, 8, 3, 1, 2 };
-          for (int i = 0; i < array.length ; i++) {
+          for (int i = 0; i < array.length; i++) {
                int smallest = i;
                for (int j = i + 1; j < array.length; j++)
 
